881c83ab440853899104844632d4caf34d1a7855
Jakub Koza <kubulax25@gmail.com>
2024-03-01 11:00:11 +0100

    Added ScoreSystem class responsible for score related operations.

---
+++ KolkoIKrzyzykJKLJ/ScoreSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KolkoIKrzyzykJKLJ
+{
+    public static class ScoreSystem
+    {
+        private static int[] score = new int[2];
+
+        public static void AddPoint(string player)
+        {
+            if(player == "×")
+            {
+                score[0]++;
+            }
+            else if(player == "○")
+            {
+                score[1]++;
+            }
+        }
+
+        public static void ResetScore(string player) 
+        {
+            score = new int[2];
+        }
+
+        public static string DrawScoreDisplay()
+        {
+            return "× " + score[0] + ":" + score[1] + " ○";
+        }
+    }
+}
